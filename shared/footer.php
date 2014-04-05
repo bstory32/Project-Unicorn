ad01ab78d9632552c5cb6cb1e7a5c71c6909c8a9
Brian Story <bstory32@gmail.com>
2014-04-04 22:02:28 -0500

    added initial footer data

--- shared/footer.php
+++ shared/footer.php
@@ -1,3 +1,50 @@
+    <!-- Start of footer -->
+    <div class="footer-bg">
+    	<footer>
+    		<div class="row">
+    			<div class="col-sm-3">
+    				<h4>title</h4>
+    				<ul>
+    					<li><a href="">link</a></li>
+    					<li><a href="">link</a></li>
+    					<li><a href="">link</a></li>
+    					<li><a href="">link</a></li>
+    					<li><a href="">link</a></li>
+    				</ul>
+    			</div>
+    			<div class="col-sm-3">
+    				<h4>title</h4>
+    				<ul>
+    					<li><a href="">link</a></li>
+    					<li><a href="">link</a></li>
+    					<li><a href="">link</a></li>
+    					<li><a href="">link</a></li>
+    					<li><a href="">link</a></li>
+    				</ul>
+    			</div>
+    			<div class="col-sm-3">
+    				<h4>title</h4>
+    				<ul>
+    					<li><a href="">link</a></li>
+    					<li><a href="">link</a></li>
+    					<li><a href="">link</a></li>
+    					<li><a href="">link</a></li>
+    					<li><a href="">link</a></li>
+    				</ul>
+    			</div>
+    			<div class="col-sm-3">
+    				<h4>Contact Us</h4>
+    				<ul>
+    					<li>Chicago, IL</li>
+    					<li>000 000 0000</li>
+    					<li>Email: start@tech5designs.com</li>
+    				</ul>
+    			</div>
+    		</div>
+    	</footer>
+    </div>
+	<!-- end of footer -->
+
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
