feb50f5a50dbd6ffe930870a44cb675e9ea98cef
Neil Sammons <neil.a.sammons@gmail.com>
2014-04-04 22:00:45 -0500

    added main message

--- index.php
+++ index.php
@@ -4,7 +4,54 @@
 
 <div class="container">
 
+<!-- Start of main header -->
+
+<div class="row">
+  <div class="col-sm-12">
+    <h1>Content Focused, Results Driven!</h1>
+    <h2>With a touch of creative vision.</h2>
+  </div>
+</div>
+
+
+<!-- end of main header -->
+
+
+<!-- start of main message -->
+
+<div class="row">
+  <div class="col-sm-12">
+    <h1>You Dream It, We Build It</h1>
+  </div>
+  <div class="row">
+  	<div class="col-sm-4">
+  		<img src="http://placekitten.com/g/300/200" alt="">
+  	</div>
+  	<div class="col-sm-4">
+  		<img src="http://placekitten.com/g/300/200" alt="">
+  	</div>
+  	<div class="col-sm-4">
+  		<img src="http://placekitten.com/g/300/200" alt="">
+  	</div>
+  </div>
+    <div class="row">
+  	<div class="col-sm-4">
+  		<img src="http://placekitten.com/g/300/200" alt="">
+  	</div>
+  	<div class="col-sm-4">
+  		<img src="http://placekitten.com/g/300/200" alt="">
+  	</div>
+  	<div class="col-sm-4">
+  		<img src="http://placekitten.com/g/300/200" alt="">
+  	</div>
+  </div>
+</div>
+
+
+<!-- end of main message -->
+
 </div><!-- /.container -->
+
 
 
 <!-- Start of footer -->
