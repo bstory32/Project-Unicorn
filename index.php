16556ab5bb9c5cccb4a2a85fefba272bf38c03e4
Neil Sammons <neil.a.sammons@gmail.com>
2014-04-06 21:49:22 -0500

    added classes for styling to homepage

--- index.php
+++ index.php
@@ -21,81 +21,121 @@
 
 
 <!-- start of main message -->
-<div class="container">
-  <div class="row">
-    <div class="col-sm-12">
-      <h1>You Dream It, We Build It</h1>
-    </div>
+
+<div class="block picture-bg">
+  <div class="container">
     <div class="row">
-     <div class="col-sm-4">
-      <img src="http://placekitten.com/g/300/200" alt="">
+      <div class="col-sm-12">
+        <h1>You Dream It, We Build It</h1>
+      </div>
+      <div class="row">
+       <div class="col-sm-3">
+        <a href="">
+          <img src="http://placekitten.com/g/200/200" alt="">
+        </a>
+      </div>
+      <div class="col-sm-3">
+        <a href="">
+          <img src="http://placekitten.com/g/200/200" alt="">
+        </a>
+      </div>
+      <div class="col-sm-3">
+        <a href="">
+          <img src="http://placekitten.com/g/200/200" alt="">
+        </a>
+      </div>
+
+      <div class="col-sm-3">
+        <a href="">
+          <img src="http://placekitten.com/g/200/200" alt="">
+        </a>
+      </div>
     </div>
-    <div class="col-sm-4">
-      <img src="http://placekitten.com/g/300/200" alt="">
+    
+    <div class="row">
+     <div class="col-sm-3">
+      <a href="">
+        <img src="http://placekitten.com/g/200/200" alt="">
+      </a>
     </div>
-    <div class="col-sm-4">
-      <img src="http://placekitten.com/g/300/200" alt="">
+    <div class="col-sm-3">
+      <a href="">
+        <img src="http://placekitten.com/g/200/200" alt="">
+      </a>
     </div>
-  </div>
-  <div class="row">
-  	<div class="col-sm-4">
-  		<img src="http://placekitten.com/g/300/200" alt="">
-  	</div>
-  	<div class="col-sm-4">
-  		<img src="http://placekitten.com/g/300/200" alt="">
-  	</div>
-  	<div class="col-sm-4">
-  		<img src="http://placekitten.com/g/300/200" alt="">
-  	</div>
+    <div class="col-sm-3">
+      <a href="">
+        <img src="http://placekitten.com/g/200/200" alt="">
+      </a>
+    </div>
+    <div class="col-sm-3">
+      <a href="">
+        <img src="http://placekitten.com/g/200/200" alt="">
+      </a>
+    </div>
   </div>
 </div>
-
-
+</div>
+</div>
+</div>
 <!-- start of contact -->
-<div class="row">
-	<div class="col-sm-12">
-		<h2>Creation Stems From Imagination!</h2>
-		<a class="btn btn-primary" href="">
-			Let Us Work With Your Vision
-		</a>
-	</div>
-</div>
 
+
+<div class="block strategy">
+  <div class="container">
+    <div class="row">
+     <div class="col-sm-12">
+      <h2>Creation Stems From Imagination!</h2>
+       <h3>Let Us Work With Your Vision</h3>
+     </a>
+   </div>
+  </div>
+  </div>
+</div>
 <!-- end of contact -->
 
 <!-- start of process section -->
 
-
-<div class="row">
-  <div class="col-sm-3">
-    <img src="http://placekitten.com/g/150/150" alt="">
-    <h3>Strategy &amp; Consulting</h3>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur, voluptate, porro in ut possimus quaerat id molestias dolores! Sed, minus nulla recusandae quos dolorum voluptate eos iure repudiandae optio sit!</p>
+<div class="process">
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-3">
+        <div class="align-center">
+          <span class="glyphicon glyphicon-tower"></span>
+        </div>
+        <h3>Strategy &amp; Consulting</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur, voluptate, porro in ut possimus quaerat id molestias dolores! Sed, minus nulla recusandae quos dolorum voluptate eos iure repudiandae optio sit!</p>
+      </div>
+      <div class="col-sm-3">
+        <div class="align-center">
+          <span class="glyphicon glyphicon-pencil"></span>
+        </div>
+        <h3>Creative Design</h3>
+        <p>Beatae, sequi, nesciunt suscipit sunt ipsam adipisci magni earum itaque culpa ad. Illo, nesciunt, iste sit dignissimos id atque eligendi illum nam eum voluptates quidem quo veniam possimus excepturi impedit.</p>
+      </div>
+      <div class="col-sm-3">
+        <div class="align-center">
+          <span class="glyphicon glyphicon-wrench"></span>
+        </div>
+        <h3>Web &amp; Mobile Development</h3>
+        <p>Similique, incidunt, illum ullam fugit fuga voluptas natus aliquam fugiat tempora repellendus nisi consequuntur eligendi esse provident ex quia impedit dignissimos porro autem odit veniam officiis ipsa magnam ab doloremque!</p>
+      </div>
+      <div class="col-sm-3">
+        <div class="align-center">
+          <span class="glyphicon glyphicon-signal"></span>
+        </div>
+        <h3>Online Marketing</h3>
+        <p>Impedit, placeat, assumenda tenetur soluta incidunt sunt explicabo cupiditate voluptatibus voluptatum quas nesciunt voluptatem veritatis. Itaque, recusandae assumenda molestias perferendis aliquid ipsum sed libero tempora eaque nisi. Libero, soluta, quidem.</p>
+      </div>
+    </div>
   </div>
-  <div class="col-sm-3">
-    <img src="http://placekitten.com/g/150/150" alt="">
-    <h3>Creative Design</h3>
-    <p>Beatae, sequi, nesciunt suscipit sunt ipsam adipisci magni earum itaque culpa ad. Illo, nesciunt, iste sit dignissimos id atque eligendi illum nam eum voluptates quidem quo veniam possimus excepturi impedit.</p>
-  </div>
-  <div class="col-sm-3">
-    <img src="http://placekitten.com/g/150/150" alt="">
-    <h3>Web &amp; Mobile Development</h3>
-    <p>Similique, incidunt, illum ullam fugit fuga voluptas natus aliquam fugiat tempora repellendus nisi consequuntur eligendi esse provident ex quia impedit dignissimos porro autem odit veniam officiis ipsa magnam ab doloremque!</p>
-  </div>
-  <div class="col-sm-3">
-    <img src="http://placekitten.com/g/150/150" alt="">
-    <h3>Online Marketing</h3>
-    <p>Impedit, placeat, assumenda tenetur soluta incidunt sunt explicabo cupiditate voluptatibus voluptatum quas nesciunt voluptatem veritatis. Itaque, recusandae assumenda molestias perferendis aliquid ipsum sed libero tempora eaque nisi. Libero, soluta, quidem.</p>
-  </div>
-</div>
+
+  <!-- end of process section -->
 
 
-<!-- end of process section -->
+  <!-- end of main message -->
 
-
-<!-- end of main message -->
-
-</div><!-- /.container -->
+</div><!-- /.process -->
 
 
 
