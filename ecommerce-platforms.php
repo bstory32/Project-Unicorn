97e189ff4b00bd371f0ba532ecd2742822e75586
Neil Sammons <neil.a.sammons@gmail.com>
2014-04-08 16:51:06 -0500

    added structure to ecommerce page

---
+++ ecommerce-platforms.php
@@ -0,0 +1,38 @@
+<!-- Start of header -->
+<?php include('shared/header.php'); ?>
+<!-- End of header -->
+
+<div class="container">
+	<div class="row">
+		<div class="col-sm-12">
+			<h1>eCommerce Platforms</h1>
+			<h2>You Got it, We'll Help Sell it!</h2>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-sm-12">
+			<span class="glyphicon glyphicon-globe"></span>
+			<h3>Title</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi, sed, eaque, nisi, ipsum eius reprehenderit id quo voluptatibus quidem pariatur eos vel impedit minima accusantium nulla alias omnis illum unde.</p>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-sm-12">
+			<span class="glyphicon glyphicon-globe"></span>
+			<h3>Title</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur, optio, maiores, dolor veniam quibusdam nesciunt hic cupiditate aperiam quis temporibus ipsam accusantium ab distinctio fugit velit voluptates voluptatum laboriosam vero.</p>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-sm-12">
+			<span class="glyphicon glyphicon-globe"></span>
+			<h3>Title</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, ex, aspernatur, delectus, qui dolor atque ullam fugiat assumenda commodi impedit iste ad neque corporis dolorem natus architecto quasi quos exercitationem.</p>
+		</div>
+	</div>
+	
+</div>
+
+<!-- Start of footer -->
+<?php include('shared/footer.php'); ?>
+<!-- End of footer -->
