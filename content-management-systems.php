2846a18fa85da803f997a8670d8eac60f5a48e4d
Neil Sammons <neil.a.sammons@gmail.com>
2014-04-09 20:47:51 -0500

    added padding to body

--- content-management-systems.php
+++ content-management-systems.php
@@ -2,28 +2,16 @@
 <?php include('shared/header.php'); ?>
 <!-- End of header -->
 
-<!-- Start of main message -->
+
+<!-- Start of list of features  -->
+
+
 
 <div class="container">
 	<div class="row">
-		<div class="col-sm-12">
+		<div class="col-sm-6">
 			<h1>Content Management Systems</h1>
-			
-			</div>
-		</div>
-	</div>
-
-	<!-- End of main message  -->
-
-
-	<!-- Start of list of features  -->
-
-
-	
-		<div class="container">
-			<div class="row">
-				<div class="col-sm-6">
-					<h1>Creation at Your Fingertips!</h1>
+			<h2>Creation at Your Fingertips!</h2>
 
 			<p>With the latest Content Management Systems(CMS), Tech 5 can structure your website 
 				that will give you:</p>
@@ -32,8 +20,8 @@
 					<li>Creativity with virtually no barriers</li>
 					<li>Flexible and Responsive CMS Interface</li>
 				</ul>
-				</div>
-				<div class="cms-features">
+			</div>
+			<div class="cms-features">
 				<div class="col-sm-3">
 					<span class="glyphicon glyphicon-pencil"></span>
 					<h2>Text Editing</h2>
@@ -44,55 +32,55 @@
 					<h2>Image Management</h2>
 					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit, eligendi, porro, distinctio nobis illo impedit ea perferendis dolore dolorum nostrum repellendus veniam incidunt iste officia vitae quibusdam ratione. Eligendi, cum.</p>
 				</div>
-			
-			<div class="row">
-				<div class="col-sm-3">
-					<span class="glyphicon glyphicon-plus"></span>
-					<h2>Multi-Tiered User Permissions</h2>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam, numquam, dolorem, voluptatibus adipisci aperiam deserunt ad accusamus recusandae repellendus illum esse aliquid debitis ipsum veniam expedita earum laudantium. Eius, quo.</p>
+				
+				<div class="row">
+					<div class="col-sm-3">
+						<span class="glyphicon glyphicon-plus"></span>
+						<h2>Multi-Tiered User Permissions</h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam, numquam, dolorem, voluptatibus adipisci aperiam deserunt ad accusamus recusandae repellendus illum esse aliquid debitis ipsum veniam expedita earum laudantium. Eius, quo.</p>
+					</div>
+					<div class="col-sm-3">
+						<span class="glyphicon glyphicon-briefcase"></span>
+						<h2>Lead Generation Web Forms</h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel, temporibus, assumenda eos eligendi provident aperiam earum perferendis modi voluptatum laudantium ratione non iure et ullam aliquam consequuntur nihil similique magni.</p>
+					</div>
+					<div class="col-sm-3">
+						<span class="glyphicon glyphicon-flash"></span>
+						<h2>Subpage Creation</h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error, dolorem, consequuntur esse similique illo eos cumque voluptatem ratione cupiditate sit optio accusamus itaque voluptate qui adipisci blanditiis amet. Beatae, eius.</p>
+					</div>
+					<div class="col-sm-3">
+						<span class="glyphicon glyphicon-transfer"></span>
+						<h2>E-commerce Functionality</h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam, eaque adipisci ipsam dolorum a vero quia. Reprehenderit, magni, assumenda blanditiis illum ut ducimus suscipit ullam veritatis laboriosam laborum facilis magnam!</p>
+					</div>
 				</div>
-				<div class="col-sm-3">
-					<span class="glyphicon glyphicon-briefcase"></span>
-					<h2>Lead Generation Web Forms</h2>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel, temporibus, assumenda eos eligendi provident aperiam earum perferendis modi voluptatum laudantium ratione non iure et ullam aliquam consequuntur nihil similique magni.</p>
-				</div>
-				<div class="col-sm-3">
-					<span class="glyphicon glyphicon-flash"></span>
-					<h2>Subpage Creation</h2>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error, dolorem, consequuntur esse similique illo eos cumque voluptatem ratione cupiditate sit optio accusamus itaque voluptate qui adipisci blanditiis amet. Beatae, eius.</p>
-				</div>
-				<div class="col-sm-3">
-					<span class="glyphicon glyphicon-transfer"></span>
-					<h2>E-commerce Functionality</h2>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam, eaque adipisci ipsam dolorum a vero quia. Reprehenderit, magni, assumenda blanditiis illum ut ducimus suscipit ullam veritatis laboriosam laborum facilis magnam!</p>
-				</div>
-			</div>
 
-			<div class="row">
-				<div class="col-sm-3">
-					<span class="glyphicon glyphicon-star"></span>
-					<h2>Compatible with 3rd Party Integration</h2>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit, ea, aspernatur eum ratione rerum aperiam voluptates harum officiis sint vero voluptatum cum quisquam error aut eveniet voluptate dolorem! Nostrum, aperiam.</p>
-				</div>
-				<div class="col-sm-3">
-					<span class="glyphicon glyphicon-star"></span>
-					<h2>Integrated Website Analytics</h2>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis, tenetur, aliquam, odio, sit quibusdam neque eos perspiciatis officiis quas quos obcaecati sunt earum placeat eius porro impedit delectus. Quis, maxime.</p>
-				</div>
-				<div class="col-sm-3">
-					<span class="glyphicon glyphicon-star"></span>
-					<h2>Contact Database</h2>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam, voluptatem, nam, animi, maxime ullam ipsam aut voluptatibus provident recusandae iure nisi odio earum tempore culpa porro ea placeat deleniti error!</p>
-				</div>
-				<div class="col-sm-3">
-					<span class="glyphicon glyphicon-star"></span>
-					<h2>Search Engine Optimization</h2>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, natus, veniam doloribus consectetur ullam minima officia accusamus eius sint deleniti iste adipisci asperiores cupiditate facere facilis aliquam delectus nesciunt alias.</p>
+				<div class="row">
+					<div class="col-sm-3">
+						<span class="glyphicon glyphicon-star"></span>
+						<h2>Compatible with 3rd Party Integration</h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit, ea, aspernatur eum ratione rerum aperiam voluptates harum officiis sint vero voluptatum cum quisquam error aut eveniet voluptate dolorem! Nostrum, aperiam.</p>
+					</div>
+					<div class="col-sm-3">
+						<span class="glyphicon glyphicon-star"></span>
+						<h2>Integrated Website Analytics</h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis, tenetur, aliquam, odio, sit quibusdam neque eos perspiciatis officiis quas quos obcaecati sunt earum placeat eius porro impedit delectus. Quis, maxime.</p>
+					</div>
+					<div class="col-sm-3">
+						<span class="glyphicon glyphicon-star"></span>
+						<h2>Contact Database</h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam, voluptatem, nam, animi, maxime ullam ipsam aut voluptatibus provident recusandae iure nisi odio earum tempore culpa porro ea placeat deleniti error!</p>
+					</div>
+					<div class="col-sm-3">
+						<span class="glyphicon glyphicon-star"></span>
+						<h2>Search Engine Optimization</h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, natus, veniam doloribus consectetur ullam minima officia accusamus eius sint deleniti iste adipisci asperiores cupiditate facere facilis aliquam delectus nesciunt alias.</p>
+					</div>
 				</div>
 			</div>
 		</div>
-	</div>
-				
+		
 
 	</div> <!-- End of list of features  --> 
 
