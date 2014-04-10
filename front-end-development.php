97fa6d0961d37bd6402c8833ac131a773a750d56
Neil Sammons <neil.a.sammons@gmail.com>
2014-04-09 21:13:23 -0500

    added html structure

--- front-end-development.php
+++ front-end-development.php
@@ -4,9 +4,28 @@
 
 <div class="container">
 	<div class="row">
-		<div class="col-sm-12">
-			<h1>Front End Development</h1>
-			<p>lorem</p>
+		<div class="col-sm-10">
+			<h1> What is Front End Development?</h1>
+			<p>Our Frontend Development Team creates strong pages incorporating HTML5, CSS, and JavaScript</p>
+			<h2>Here is what our Frontend Development Team Does</h2>
+			<ul>
+				<li>Graphic design and image production</li>
+				<li>Interface design</li>
+				<li>Information design as it pertains to the user's experience of the site</li>
+				<li>HTML document and style sheet development</li>
+				<li>JavaScript</li>
+			</ul>
+		</div>
+		<div class="col-sm-2">
+			<aside>
+				<h3>Our Work</h3>
+				<ul>
+					<li>Hey</li>
+					<li>How</li>
+					<li>Are</li>
+					<li>Ya</li>
+				</ul>
+			</aside>
 		</div>
 	</div>
 </div>
