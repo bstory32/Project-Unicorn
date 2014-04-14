1ed1c995f42e30d459d28fb934014252fb07ae85
Neil Sammons <neil.a.sammons@gmail.com>
2014-04-14 17:41:48 -0500

    removed paragrpahs from cms page

--- content-management-systems.php
+++ content-management-systems.php
@@ -25,34 +25,34 @@
 				<div class="col-sm-3">
 					<span class="glyphicon glyphicon-pencil"></span>
 					<h2>Text Editing</h2>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore, expedita doloribus consectetur. Nam, ipsum iste atque id voluptate vel nihil soluta velit aliquid cum commodi sequi rerum non dicta fugit!</p>
+					<p></p>
 				</div>
 				<div class="col-sm-3">
 					<span class="glyphicon glyphicon-camera"></span>
 					<h2>Image Management</h2>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit, eligendi, porro, distinctio nobis illo impedit ea perferendis dolore dolorum nostrum repellendus veniam incidunt iste officia vitae quibusdam ratione. Eligendi, cum.</p>
+					<p></p>
 				</div>
 				
 				<div class="row">
 					<div class="col-sm-3">
 						<span class="glyphicon glyphicon-plus"></span>
 						<h2>Multi-Tiered User Permissions</h2>
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam, numquam, dolorem, voluptatibus adipisci aperiam deserunt ad accusamus recusandae repellendus illum esse aliquid debitis ipsum veniam expedita earum laudantium. Eius, quo.</p>
+						<p></p>
 					</div>
 					<div class="col-sm-3">
 						<span class="glyphicon glyphicon-briefcase"></span>
 						<h2>Lead Generation Web Forms</h2>
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel, temporibus, assumenda eos eligendi provident aperiam earum perferendis modi voluptatum laudantium ratione non iure et ullam aliquam consequuntur nihil similique magni.</p>
+						<p></p>
 					</div>
 					<div class="col-sm-3">
 						<span class="glyphicon glyphicon-flash"></span>
 						<h2>Subpage Creation</h2>
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error, dolorem, consequuntur esse similique illo eos cumque voluptatem ratione cupiditate sit optio accusamus itaque voluptate qui adipisci blanditiis amet. Beatae, eius.</p>
+						<p></p>
 					</div>
 					<div class="col-sm-3">
 						<span class="glyphicon glyphicon-transfer"></span>
 						<h2>E-commerce Functionality</h2>
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam, eaque adipisci ipsam dolorum a vero quia. Reprehenderit, magni, assumenda blanditiis illum ut ducimus suscipit ullam veritatis laboriosam laborum facilis magnam!</p>
+						<p></p>
 					</div>
 				</div>
 
@@ -60,22 +60,22 @@
 					<div class="col-sm-3">
 						<span class="glyphicon glyphicon-star"></span>
 						<h2>Compatible with 3rd Party Integration</h2>
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit, ea, aspernatur eum ratione rerum aperiam voluptates harum officiis sint vero voluptatum cum quisquam error aut eveniet voluptate dolorem! Nostrum, aperiam.</p>
+						<p></p>
 					</div>
 					<div class="col-sm-3">
 						<span class="glyphicon glyphicon-star"></span>
 						<h2>Integrated Website Analytics</h2>
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis, tenetur, aliquam, odio, sit quibusdam neque eos perspiciatis officiis quas quos obcaecati sunt earum placeat eius porro impedit delectus. Quis, maxime.</p>
+						<p></p>
 					</div>
 					<div class="col-sm-3">
 						<span class="glyphicon glyphicon-star"></span>
 						<h2>Contact Database</h2>
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam, voluptatem, nam, animi, maxime ullam ipsam aut voluptatibus provident recusandae iure nisi odio earum tempore culpa porro ea placeat deleniti error!</p>
+						<p></p>
 					</div>
 					<div class="col-sm-3">
 						<span class="glyphicon glyphicon-star"></span>
 						<h2>Search Engine Optimization</h2>
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, natus, veniam doloribus consectetur ullam minima officia accusamus eius sint deleniti iste adipisci asperiores cupiditate facere facilis aliquam delectus nesciunt alias.</p>
+						<p></p>
 					</div>
 				</div>
 			</div>
