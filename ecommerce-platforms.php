f3b81b3093b595f6daa008e9dd49445a4ecbb989
Neil Sammons <neil.a.sammons@gmail.com>
2014-04-14 19:33:31 -0500

    added main structure to ecommerce page

--- ecommerce-platforms.php
+++ ecommerce-platforms.php
@@ -10,56 +10,56 @@
 		</div>
 	</div>
 	<div class="row">
-		<div class="col-sm-8">
-			<h3>What do you need to know?</h3>
-			<span class="glyphicon glyphicon-globe"></span>
-			
-		</div>
-		
-		<div class-"col-sm-4">
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi, sed, eaque, nisi, ipsum eius reprehenderit id quo voluptatibus quidem pariatur eos vel impedit minima accusantium nulla alias omnis illum unde.</p>
-		</div>
-	</div>
-	<div class="row">
-		<div class="col-sm-8">
-			<h3>Do you know what your selling?</h3>
-			<span class="glyphicon glyphicon-globe"></span>
-			
-		</div>
-		
-		<div class-"col-sm-4">
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi, sed, eaque, nisi, ipsum eius reprehenderit id quo voluptatibus quidem pariatur eos vel impedit minima accusantium nulla alias omnis illum unde.</p>
-		</div>
-	</div>
-	<div class="row">
-		<div class="col-sm-8">
-			<h3>Do you know what shopping options you plan to offer?</h3>
-			<span class="glyphicon glyphicon-globe"></span>
-			
-		</div>
-		
-		<div class-"col-sm-4">
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi, sed, eaque, nisi, ipsum eius reprehenderit id quo voluptatibus quidem pariatur eos vel impedit minima accusantium nulla alias omnis illum unde.</p>
+		<div class="col-sm-12">
+			<h2>Why you need to sell online</h2>
+			<p>There is no better time to sell online then now.  Being able to sell your product online allows you to reach a larger audience wihout being subjected to your local area.</p>
 		</div>
 	</div>
 
-	<div class="row">
-		<div class="col-sm-8">
-			<h3>Do you know what payment options are available?</h3>
-			<span class="glyphicon glyphicon-globe"></span>
-			
-			
-			
+	<div class="information-message">
+		<div class="row">
+			<div class="col-sm-4">
+				<h3>What do you need to know?</h3>
+				<img src="http://i.istockimg.com/file_thumbview_approve/18595530/2/stock-photo-18595530-speech-balloon.jpg" alt="">
+				<ul>
+					<li>Know how your business fits into your market</li>
+					<li>Know your niche and market accordingly</li>
+					<li>Know and rank your business intiatives, while evaluating annualy</li>
+					<li>Invest in a solid web design</li>
+					<li>Track customer activity</li>
+					<li>Maintain excellent customer service</li>
+					<li>Use the right technology to your advantage</li>
+				</ul>
+			</div>
 
-		</div>
-		
-		<div class-"col-sm-4">
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi, sed, eaque, nisi, ipsum eius reprehenderit id quo voluptatibus quidem pariatur eos vel impedit minima accusantium nulla alias omnis illum unde.</p>
+
+			<div class="col-sm-4">
+				<h3>How to Get Started?</h3>
+				<img src="http://i.istockimg.com/file_thumbview_approve/37519738/2/stock-photo-37519738-what-is-next.jpg" alt="">
+				<ul>
+					<li>Prepare for the work involved</li>
+					<li>Have a business plan</li>
+					<li>Have a marketing startegy</li>
+					<li>Have the proper ecommerce platform for your business</li>
+					<li>Showcase your products</li>
+
+				</ul>
+			</div>
+
+			<div class="col-sm-4">
+				<h3>What Tech 5 Offers</h3>
+				<img src="http://i.istockimg.com/file_thumbview_approve/15670664/2/stock-photo-15670664-solutions-puzzle-in-blue.jpg" alt="">
+				<ul>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+				</ul>
+
+			</div>
+
 		</div>
 	</div>
-	
-</div>
-
-<!-- Start of footer -->
-<?php include('shared/footer.php'); ?>
+	<!-- Start of footer -->
+	<?php include('shared/footer.php'); ?>
 <!-- End of footer -->
