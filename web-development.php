378ebe4fc511b99b1fbd4ece6fdcaefa739ab1e4
Neil Sammons <neil.a.sammons@gmail.com>
2014-04-15 22:42:00 -0500

    added web development page structure

--- web-development.php
+++ web-development.php
@@ -5,9 +5,51 @@
 <div class="container">
 	<div class="row">
 		<div class="col-sm-12">
-			<h1>Title</h1>
+			<h1>Our Web Development</h1>
+			<h2>No matter the project, Tech 5 is ready to work with you</h2>
 		</div>
 	</div>
+
+	<div class="row">
+		<div class="col-sm-8">
+			<h3>Web Applications</h3>
+			<p> Here at Tech 5, we program our amazing sites with the combination of HTML5, CSS, and JavaScript to give you an awesome responsive design that brings results.
+				Working with the latest web applications and technology, here is a short list of what we work with.</p>
+				<ul>
+					<li></li>
+					<li></li>
+					<li></li>
+				</ul>
+		</div>
+
+		<div class="col-sm-4">
+			<img src="http://i.istockimg.com/file_thumbview_approve/27379161/2/stock-photo-27379161-business-teamwork-internet-and-communication-concept.jpg" alt="">
+		</div>
+	</div>
+
+<div class="row">
+	<div class="col-sm-4">
+		<img src="http://i.istockimg.com/file_thumbview_approve/27379161/2/stock-photo-27379161-business-teamwork-internet-and-communication-concept.jpg" alt="">
+	</div>
+	<div class="col-sm-8">
+		<h3>Brochure Sites</h3>
+		<p>Every business small to large has an image that begs for orginality.  Tech 5 can build what you envision.  With our team of excellent front and backend designers, we can build what you dream.
+		Let us combine our passion and imagination with yours, to make your website worth remembering!  Here are a few examples</p>
+		<button class="btn btn-primary">Our Work</button>
+	</div>
+</div>
+
+<div class="row">
+	<div class="col-sm-8">
+		<h3>Our Process</h3>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique, illum, tempore tenetur alias optio nihil dicta molestias aliquam tempora quos sed iure praesentium. Quibusdam error modi consequuntur sit numquam autem.</p>
+	</div>
+
+<div class="col-sm-4">
+	<img src="http://i.istockimg.com/file_thumbview_approve/27379161/2/stock-photo-27379161-business-teamwork-internet-and-communication-concept.jpg" alt="">
+</div>
+</div>
+
 </div>
 
 
