8fb09d5662e8019f3e6d96a305da99f32353b998
Neil Sammons <neil.a.sammons@gmail.com>
2014-04-15 22:41:28 -0500

    added contact page structure

--- contact.php
+++ contact.php
@@ -4,12 +4,114 @@
 
 <div class="container">
 	<div class="row">
-		<div class="col-sm-12">
-			<h1>Title</h1>
+		<div class="col-sm-6">
+			<h1>Lets get the conversation started!</h1>
+			<p>Telling us a little bit about your ideal website will help us put the pieces in place. Fill out this form and we'll call you up soon.</p>
+			<h3>We partner with businesses and creative thinkers to build amazing web and mobile projects.</h3>
+			<p>Our goal is to make the internet a better place. We do this by building great website that focus on content and user experience. Here is a list of services that we offer</p>
+			<ul>
+				<li>Web Design</li>
+				<li>Logo Design</li>
+				<li>Mobile Web Design</li>
+				<li>Interactive web Applications</li>
+				<li>Social Media Templates</li>
+				<li>Internet Marketing</li>
+				<li>Custom Internet Applications - You dream it and we build it</li>
+			</ul>
+			<h4>In Addition to those things we also do</h4>
+			<ul>
+				<li>Web Hosting</li>
+				<li>Domain Registration</li>
+				<li>Site Management</li>
+			</ul>
+		</div>
+
+		<div class="contact-form">
+			<div class="col-sm-6">
+				<h2>Tell Us to Contact You</h2>
+				<form role="form">
+					<div class="form-group">
+						<label for="exampleInputEmail1">Name</label>
+						<input type="email" class="form-control" id="exampleInputEmail1" placeholder="What's your name?">
+					</div>
+					<div class="form-group">
+						<label for="exampleInputPassword1">Company</label>
+						<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Do you have a company?">
+					</div>
+					<div class="form-group">
+						<label for="exampleInputPassword1">Phone Number</label>
+						<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Tell me your number!">
+					</div>
+					<div class="form-group">
+						<label for="exampleInputPassword1">Email Address</label>
+						<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Who has email addresses?">
+					</div>
+					<div class="form-group">
+						<label for="exampleInputPassword1">Current URL - If you have one</label>
+						<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Show me your old website, now!">
+					</div>
+
+
+					<button type="submit" class="btn btn-default">Lets Do It</button>
+				</form>
+
+			</div>
+
+		</div>
+			<!-- <div class="span4">
+
+				<div class="contact_form">
+					<h3>Tell us to contact you</h3>
+					<form  action="contact.php" method="POST">
+						<div class="control-group">
+							<div class="controls">
+								<div class="input-prepend">
+									<span class="glyphicon glyphicon-user"></span>
+									<input class="span3"  type="text" name="contact_name" placeholder="Name" required>
+								</div>
+							</div>
+							<div class="controls">
+								<div class="input-prepend">
+									<span class="glyphicon glyphicon-home"></span>
+									<input class="span3"  type="text" name="contact_company" placeholder="Company">
+								</div>
+							</div>
+							<div class="controls">
+								<div class="input-prepend">
+									<span class="glyphicon glyphicon-thumbs-up"></span>
+									<input class="span3" type="tel" name="contact_phone"  placeholder="Phone Number" required>
+								</div>
+							</div>
+							<div class="controls">
+								<div class="input-prepend">
+									<span class="glyphicon glyphicon-envelope"></span>
+									<input class="span3" type="email"  name="contact_email" placeholder="Email Address" required>
+								</div>
+							</div>
+							<div class="controls">
+								<div class="input-prepend">
+									<span class="glyphicon glyphicon-globe"></span>
+									<input class="span3" type="text"  name="contact_url" placeholder="Current URL- If you have one">
+								</div>
+							</div>
+							<div class="controls">
+								<label>What are the project details?</label>
+								<div class="input-prepend">
+									<span class="glyphicon glyphicon-comment"></span>
+									<textarea class="span3" rows="5"  name="contact_about" placeholder="I need a simple website for my company that includes a blog, contact form, and a new logo! We pay in ice cream."></textarea>
+								</div>
+							</div>
+							<button class="btn btn-large btn-success span3" type="submit" value="send">Lets Do It</button>
+						</div>
+
+					</form>
+
+				</div>
+			</div> -->
+
 		</div>
 	</div>
-</div>
 
-<!-- Start of footer -->
-<?php include('shared/footer.php'); ?>
-<!-- End of footer -->
+	<!-- Start of footer -->
+	<?php include('shared/footer.php'); ?>
+<!-- End of footer
